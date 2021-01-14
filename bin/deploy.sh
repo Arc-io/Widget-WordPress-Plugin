51151b0e86db3bd0fdd52caa8ebc13631ff6bb06
Eric Guan <guanzo91@gmail.com>
2021-01-14 13:28:03 -0800

    deploy script finds version to deploy from trunk/arc.php instead of from script args

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -13,50 +13,44 @@ projectDir="$scriptDir/../"
 
 cd "$projectDir"
 
-if (( "$#" != 1 ))
-then
-    echo "Usage: ./deploy.sh <version>"
-    echo "<version> should be a version number, like 1.3.2"
-    exit 1
-fi
-
-version=$1
-
-rx='^([0-9]+\.){0,2}(\*|[0-9]+)$'
-if [[ ! $version =~ $rx ]]
-then
-    echo "Version is not in semver format: '$version'"
-    exit 1
-fi
-
-if [ -d "tags/"$version"" ]
-then
-    echo "Version $version is already deployed."
-    exit 1
-fi
-
 trunkVersionLine=$(grep '^* Version' trunk/arc.php)
 trunkVersion=${trunkVersionLine/* Version: /''}
 
 readmeVersionLine=$(grep '^Stable tag' trunk/readme.txt)
 readmeVersion=${readmeVersionLine/Stable tag: /''}
 
-if [[ "$version" != "$trunkVersion" || "$version" != "$readmeVersion" ]]
+semverRegex='^([0-9]+\.){0,2}(\*|[0-9]+)$'
+
+if [[ ! $trunkVersion =~ $semverRegex ]]
+then
+    echo "Version is not in semver format: '$trunkVersion'"
+    exit 1
+elif [ -d "tags/"$trunkVersion"" ]
+then
+    echo "Version $trunkVersion is already deployed."
+    exit 1
+elif [[ "$trunkVersion" != "$readmeVersion" ]]
 then
     echo "Version mismatch detected"
     echo
-    echo "1. Ensure that the \"Version:\" in trunk/arc.php and \"Stable tag:\" in trunk/readme.txt are both $version."
+    echo "1. Ensure that \"Version:\" in trunk/arc.php and \"Stable tag:\" in trunk/readme.txt are both $trunkVersion."
     echo "2. Commit and push to the remote git repo."
     echo "3. Run this script again."
     exit 1
 fi
 
-echo "Deploying version $version to svn."
+read -p "Deploy version $trunkVersion [y/n]" -n 1 -r
+echo
 
-svn cp trunk tags/"$version"
+if [[ ! $REPLY =~ ^[Yy]$ ]]
+then
+    exit 1
+fi
 
-svn ci -m "Tagging version $version"
+svn cp trunk tags/"$trunkVersion"
+
+svn ci -m "Tagging version $trunkVersion"
 
 echo
 echo
-echo ">> Arc Wordpress Plugin deployed successfully. <<"
+echo ">> Arc Wordpress Plugin version $trunkVersion deployed successfully. <<"
